e7237c1b9a356e8b36074bbd9f227eaf6bfa0d2e
unknown <annettabel@rambler.ru>
2024-01-11 18:55:01 +0500

    add title with text

--- farm-slider/app/page.tsx
+++ farm-slider/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      farm
+      <h1 className={styles.title}>Полезные материалы</h1>
+      <p className={styles.textBold}>
+        Собрали для вас полезные исследования схемы кормления и другие
+        материалы, которые пригодятся для лучших результатов на вашем хозяйстве
+      </p>
     </main>
-  )
+  );
 }
